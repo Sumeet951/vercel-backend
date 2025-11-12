17d1c47d0a98f7c3c06d2176220dde2846329519
Sumeet951 <sg297979@gmail.com>
2025-11-12 18:20:54 +0530

    fix

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 import app from "./app.js";
 import databaseconnect from "./config/dbConnection.js";
 const PORT = 3000;
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
 app.listen(PORT, async () => {
   await databaseconnect();
   console.log(`App is running at http://localhost:${PORT}`);
